abcb49f9693bd93ac0710f8c86211c4b17e67803
eisho.uchikakoi <eisho.uchikakoi@broadleaf.co.jp>
2024-09-08 23:10:16 +0900

    footerのデザインを微調整、h系marginを0に修正

--- wp/seribune/footer.php
+++ wp/seribune/footer.php
@@ -3,7 +3,11 @@
 <footer>
   <div class="ft-content">
     <p class="ft-title">水俣市競り舟協会</p>
-    <img src="<?php echo get_template_directory_uri(); ?>/images/Instagram_Glyph_Gradient.webp" alt="" class="sns-img">
+    <div class="tf-sns">
+        <a href="https://www.instagram.com/minamata.seribune/" target="_blank" rel="noreferrer noopener">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/Instagram_Glyph_Gradient.webp" alt="">
+        </a>
+      </div>
   </div>
 </footer>
 
